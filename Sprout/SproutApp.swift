46138064d426a067219ad6f607fd3c3c5862ee38
Sam Wu <samuel.wu678@gmail.com>
2025-02-13 10:26:59 -0500

    Initial Commit

---
+++ Sprout/SproutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SproutApp.swift
+//  Sprout
+//
+//  Created by Samuel Wu (student LM) on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SproutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
