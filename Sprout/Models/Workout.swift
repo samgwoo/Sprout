e319195c16bbeb1aab3766414497d2ed47e267dc
Neal Ahuja <nealahuja777@gmail.com>
2025-04-07 15:05:43 -0400

    added workout

--- Sprout/Models/Workout.swift
+++ Sprout/Models/Workout.swift
@@ -2,7 +2,26 @@
 //  Workout.swift
 //  Sprout
 //
-//  Created by Neal Ahuja (student LM) on 4/3/25.
+//  Created by Neal Ahuja (student LM) on 4/7/25.
 //
 
-import Foundation
+import SwiftUI
+
+class Workout: ObservableObject, Identifiable {
+    let id = UUID()
+    let name: String
+    let cat: String
+    var set: Int
+    @Published var rep: [Int]
+    @Published var weight: [Int]
+
+
+    init(name: String, cat: String, set: Int, rep: [Int], weight: [Int]) {
+        self.name = name
+        self.cat = cat
+        self.set = set
+        self.rep = rep
+        self.weight = weight
+    }
+}
+
--- Sprout/Models/WorkoutHistoryEntry.swift
+++ Sprout/Models/WorkoutHistoryEntry.swift
@@ -1,8 +1,14 @@
 //
 //  WorkoutHistoryEntry.swift
-//  Sprout
+//  Test
 //
-//  Created by Neal Ahuja (student LM) on 4/3/25.
+//  Created by Neal Ahuja (student LM) on 3/6/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct WorkoutHistoryEntry: Identifiable {
+    var id = UUID()
+    var date: Date
+    var workout: Workout
+}
